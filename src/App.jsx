cadda8bd64677e53f86960751e7b5bd4a6e15be4
4stron <vkallankari@gmail.com>
2024-09-16 15:05:20 +0300

    added comments to code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [checked, setChecked] = useState(false);
 
+  //Sends an alert if checkbox is clicked.
   const handleChange = () => {
     setChecked(!checked);
     !checked ?
@@ -13,26 +14,31 @@ function App() {
       alert("The product quantity wont be reset now if the product is changed.")
   };
 
+  //Adds 1 to quantity
   function increment() {
     setCounter(counter + 1);
   }
 
+  //Adds 5 to quantity
   function incrementfive() {
     setCounter(counter + 5);
   }
 
-  function decrementfive() {
-    if (counter > 4) {
-      setCounter(counter - 5);
-    }
-  }
-
+  //Deducts 1 from quantity
   function decrement() {
     if (counter > 0) {
       setCounter(counter - 1);
     }
   }
 
+  //Deducts 5 from quantity 
+  function decrementfive() {
+    if (counter > 4) {
+      setCounter(counter - 5);
+    }
+  }
+
+  //Sets new product if changed and resets quantity to 0 if checkbox is checked.
   function handleProductChange(event) {
     const newProduct = event.target.value;
     if (newProduct !== selectedProduct && checked) {    
@@ -41,10 +47,12 @@ function App() {
     setSelectedProduct(newProduct);
   }
 
+  //Resets counter
   function resetCounter() {
     setCounter(0);
   }
 
+  //Calculates total product price
   function calculateTotal() {
     const productPrice = getProductPrice(selectedProduct);
     if (productPrice) {
@@ -53,6 +61,7 @@ function App() {
     return 0;
   }
 
+  //Product prices
   function getProductPrice(productName) {
     const productPrices = {
       "AMD Ryzen 5 3600": 245,
@@ -75,8 +84,7 @@ function App() {
         <select id="product" onChange={handleProductChange}>
           <option value="">Select a product</option>
           <option value="AMD Ryzen 5 3600">AMD Ryzen 5 3600 (245€)</option>
-          <option value="intel i5 9600k">intel i5 9600k (199€)</option>
-          
+          <option value="intel i5 9600k">intel i5 9600k (199€)</option>          
         </select>
         <input
           type="checkbox"
